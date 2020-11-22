d79b798500598b7a80f311b5b5030e1d3e9683b7
Jorge Costa <jorge.costa@trimble.com>
2020-11-22 10:37:06 +0200

    fix code analysis

--- src/Atlassian.Stash/Entities/Repository.cs
+++ src/Atlassian.Stash/Entities/Repository.cs
@@ -2,15 +2,6 @@
 
 namespace Atlassian.Stash.Entities
 {
-    public class Sizes
-    {
-        [JsonProperty("repository")]
-        public long Repository { get; set; }
-
-        [JsonProperty("attachments")]
-        public long Attachments { get; set; }
-    }
-
     public class Repository
     {
         [JsonProperty("slug")]
